a4f5d8386878e91e995918011c745de02d24bd9f
Octav <octav@peoplepost.com>
2016-05-27 09:23:33 +0300

    changes

--- client/gdx/desktop/src-common/gdx/core.clj
+++ client/gdx/desktop/src-common/gdx/core.clj
@@ -1,9 +1,10 @@
 (ns gdx.core
   (:require [play-clj.core :refer :all]
-            [play-clj.ui :refer :all]))
+            [play-clj.ui :refer :all]
+            [cheshire.core :refer :all]))
 
 (require '[clojure.string :as str])
-(import com.badlogic.gdx.scenes.scene2d.utils.ChangeListener 
+(import com.badlogic.gdx.scenes.scene2d.utils.ChangeListener
         com.badlogic.gdx.scenes.scene2d.EventListener
         com.badlogic.gdx.scenes.scene2d.ui.Dialog
         [play_clj.entities ActorEntity])
@@ -12,12 +13,32 @@
 
 (def skin-path "uiskin.json")
 
-(defn do-login [email pass]  (if (or (str/blank? email) (str/blank? pass)) 
-                               "Email or password is empty"
-                               (do 
-                                 (prn email)
-                                 (prn pass) 
-                                 "")))
+(def login-url "http://localhost:9000/login")
+
+(defn start-with-user [user]
+
+  )
+
+(defn do-login [ctrl email pass]
+  (do
+    (dialog! ctrl :cancel)
+  (if (or (str/blank? email) (str/blank? pass))
+    "Email or password is empty"
+    (let [
+          listener (reify com.badlogic.gdx.Net$HttpResponseListener
+                     (handleHttpResponse [this response](
+                                                   (println (parse-string (.getResultAsString response) )))))
+          json (generate-string {:login email :password pass})
+          request (doto
+                    (com.badlogic.gdx.Net$HttpRequest. com.badlogic.gdx.Net$HttpMethods/POST)
+                    (.setUrl login-url)
+                    (.setHeader "Content-Type" "application/json")
+                    (.setHeader "Accept" "application/json")
+                    (.setContent  json) ) ]
+      (do
+        (net! :send-http-request request listener)
+        ""))))
+  )
 
 (defscreen main-screen
   :on-show
@@ -28,9 +49,9 @@
           txtEmail (text-field "" ui-skin)
           txtPass (text-field "" ui-skin :set-password-mode true :set-password-character \*)
           lblError (label "" ui-skin)
-          login-dialog (ActorEntity. (proxy [Dialog] ["Login" ui-skin] 
-                                       (result [stuff] (let [result (do-login (text-field! txtEmail :get-text) 
-                                                                 (text-field! txtPass :get-text))]  
+          login-dialog (ActorEntity. (proxy [Dialog] ["Login" ui-skin]
+                                       (result [stuff] (let [result (do-login this (text-field! txtEmail :get-text)
+                                                                 (text-field! txtPass :get-text))]
                                                          (when-not (str/blank? result)
                                                            (dialog! this :cancel)
                                                            (label! lblError :set-text result))))))
@@ -42,14 +63,14 @@
       (cell! (table! content-table :add "Login:") :align (align :right))
       (cell! (table! content-table :add (:object txtEmail)) :width 200)
       (table! content-table :row)
-      (cell! (table! content-table :add "Password:") :align (align :right)) 
+      (cell! (table! content-table :add "Password:") :align (align :right))
       (cell! (table! content-table :add (:object txtPass)) :width 200)
       (table! content-table :row)
       (cell! (table! content-table :add (:object lblError)) :colspan 2)
       (dialog! login-dialog :show (stage))
       (dialog! login-dialog :pack)
       login-dialog))
-  
+
   :on-render
   (fn [screen entities]
     (clear!)
@@ -65,12 +86,8 @@
   :on-create
   (fn [this]
     (set-screen! this main-screen)))
- 
 
 (in-ns 'gdx.core)
 (use 'play-clj.repl)
-;(gdx.core.desktop-launcher/-main)
-(when 
-  (and (not (nil? gdx-game)) 
-       (not (nil? main-screen)))
-  (on-gl (set-screen! gdx-game main-screen)))
+(gdx.core.desktop-launcher/-main)
+ (on-gl (set-screen! gdx-game main-screen))
